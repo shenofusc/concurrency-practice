61ea03f14d9074dcb9b6f2fb7a14d465a357baf3
shenofusc <shenofusc@gmail.com>
2020-06-18 23:51:00 +0800

    添加了一个演示Object.wait()和Object.notify()的例子

---
+++ src/examples/SimpleWN.java
@@ -0,0 +1,44 @@
+package examples;
+
+/**
+ * 演示了Object.wait()和Object.notify()的加锁和解锁场景，T2执行notify()之后，在wait()方法上阻塞的T1必须先获取锁之后才能继续执行
+ */
+public class SimpleWN {
+    static final Object object = new Object();
+    public static class T1 extends Thread {
+        @Override
+        public void run() {
+            synchronized (object) {
+                System.out.println(System.currentTimeMillis() + ":T1 start!");
+                try {
+                    System.out.println(System.currentTimeMillis() + ":T1 wait for object");
+                    object.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(System.currentTimeMillis() + ":T1 ended!");
+            }
+        }
+    }
+    public static class T2 extends Thread {
+        @Override
+        public void run() {
+            synchronized (object) {
+                System.out.println(System.currentTimeMillis() + ":T2 start! notify one thread");
+                object.notify();
+                System.out.println(System.currentTimeMillis() + ":T2 ended!");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new T1();
+        Thread t2 = new T2();
+        t1.start();
+        t2.start();
+    }
+}

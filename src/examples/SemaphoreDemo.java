8a9bbe1b71abfeda7f427cf0b54e06dbcc77422d
shenofusc <shenofusc@gmail.com>
2020-06-23 00:34:35 +0800

    示例代码

---
+++ src/examples/SemaphoreDemo.java
@@ -0,0 +1,34 @@
+package examples;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by shenhy on 6/23/20.
+ */
+public class SemaphoreDemo implements Runnable {
+    final Semaphore semaphore = new Semaphore(5);
+
+    @Override
+    public void run() {
+        try {
+            semaphore.acquire();
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getId() + " done");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(20);
+        final SemaphoreDemo demo = new SemaphoreDemo();
+        for (int i = 0; i < 20; i++) {
+            exec.submit(demo);
+        }
+        exec.shutdown();
+    }
+}
---
+++ src/examples/TimeLock.java
@@ -0,0 +1,36 @@
+package examples;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 指定时间内抢不到锁时放弃加锁
+ */
+public class TimeLock implements Runnable {
+    public static ReentrantLock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        try {
+            if (lock.tryLock(5, TimeUnit.SECONDS)) {
+                Thread.sleep(6000);
+            } else {
+                System.out.println("get lock failed");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TimeLock timeLock = new TimeLock();
+        Thread t1 = new Thread(timeLock);
+        Thread t2 = new Thread(timeLock);
+        t1.start();
+        t2.start();
+    }
+}
